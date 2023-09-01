b2aaf9fac5712cf521c64c3a9502f4d954fb6740
Marcelo de Castro <marcelodecastro.ms@gmail.com>
2023-09-01 20:03:20 -0300

    fix: simulate database

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,5 +1,5 @@
 import { ReactNode, useEffect, useState, useCallback } from "react";
-import { api } from "../lib/axios";
+// import { api } from "../lib/axios";
 import { createContext } from "use-context-selector";
 
 interface TransactionContextType {
@@ -23,30 +23,83 @@ interface TransactionsProviderProps {
     children: ReactNode
 }
 
+const mockedTransactions: Transactions[] = [
+    {
+        id: 1,
+        description: 'Desenvolvimento de site',
+        type: 'income',
+        category: 'Desenvolvimento',
+        price: 8000,
+        createdAt: new Date('2023-08-15')
+    },
+    {
+        id: 2,
+        description: 'Hamburguer',
+        type: 'outcome',
+        category: 'Alimentação',
+        price: 45,
+        createdAt: new Date('2023-08-15')
+    },
+    {
+        id: 3,
+        description: 'Desenvolvimento de aplicativo',
+        type: 'income',
+        category: 'Desenvolvimento',
+        price: 7000,
+        createdAt: new Date('2023-08-16')
+    },
+    {
+        id: 4,
+        description: 'Pizza',
+        type: 'outcome',
+        category: 'Alimentação',
+        price: 53,
+        createdAt: new Date('2023-08-16')
+    }
+];
+
 export const TransactionsContext = createContext({} as TransactionContextType);
 
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
     const [transactions, setTransactions] = useState<Transactions[]>([]);
+    const [transactionsTable, setTransactionsTable] = useState<Transactions[]>(mockedTransactions);
 
     const getTransactions = useCallback(async (query?: string) => {
-        const response = await api.get('transactions', {
-            params: {
-                _sort: 'createdAt',
-                _order: 'desc',
-                q: query
-            }
-        })
+        // const response = await api.get('transactions', {
+        //     params: {
+        //         _sort: 'createdAt',
+        //         _order: 'desc',
+        //         q: query
+        //     }
+        // })
 
-        setTransactions(response.data);
-    }, [])
+        // setTransactions(response.data);
+
+        const filteredTransactions = query
+            ? transactionsTable.filter(({ description, category }) => 
+                description.toLowerCase().includes(query.toLowerCase())
+                || category.toLowerCase().includes(query.toLowerCase())
+            )
+            : transactionsTable;
+
+        setTransactions(filteredTransactions);
+    }, [transactionsTable])
 
     const createTransaction = useCallback(async (data: CreateTransactionParams) => {
-        const response = await api.post('transactions', {
+        const newTransaction = {
             ...data,
             createdAt: new Date()
-        });
+        };
 
-        setTransactions(state => [response.data, ...state])
+        // const response = await api.post('transactions', newTransaction);
+
+        // setTransactions(state => [response.data, ...state]);        
+
+        setTransactionsTable(state => {
+            const id = state[state.length - 1].id + 1;
+
+            return [...state, {id, ...newTransaction}];
+        });
     }, [])
 
     useEffect(() => {
